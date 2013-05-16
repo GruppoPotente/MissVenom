5beb428ef75abe2a191772028793df239572328b
Max Kovaljov <shirioko@gmail.com>
2013-05-16 20:52:47 +0200

    Added some explanation

--- MissVenom/Program.cs
+++ MissVenom/Program.cs
@@ -17,6 +17,12 @@ namespace MissVenom
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            
+            //context class:
+            //I've embedded some DLL's into the EXE,
+            //and in order to resolve them during runtime
+            //you need a context, which the static Program
+            //class doesn't have
             Context c = new Context();
             c.Run();
         }
