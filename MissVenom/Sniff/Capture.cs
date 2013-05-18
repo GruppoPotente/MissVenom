cf1ec0c1c4bb18321f97dc0a92bf8b69fb88d904
Max Kovaljov <shirioko@gmail.com>
2013-05-18 14:07:48 +0200

    Full check-in to switch dev machines
    Continuing on my craptop this weekend

---
+++ MissVenom/Sniff/Capture.cs
@@ -0,0 +1,23 @@
+﻿using PcapDotNet.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MissVenom.Sniff
+{
+    class Capture
+    {
+        public Capture()
+        {
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+            if (allDevices.Count == 0)
+            {
+                throw new Exception("No capture devices found");
+            }
+
+            //and so on
+        }
+        
+    }
+}
