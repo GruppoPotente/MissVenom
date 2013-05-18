3039f78cab7e041aeda7bfa6512aa886676fe3e4
Max Kovaljov <shirioko@gmail.com>
2013-05-18 22:01:22 +0200

    Forcing you to run as administrator

--- MissVenom/Program.cs
+++ MissVenom/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,13 +21,42 @@ namespace MissVenom
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            //context class:
-            //I've embedded some DLL's into the EXE,
-            //and in order to resolve them during runtime
-            //you need a context, which the static Program
-            //class doesn't have
-            Context c = new Context();
-            c.Run();
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!hasAdministrativeRight)
+            {
+                RunElevated(Application.ExecutablePath);
+                Application.Exit();
+            }
+            else
+            {
+                //context class:
+                //I've embedded some DLL's into the EXE,
+                //and in order to resolve them during runtime
+                //you need a context, which the static Program
+                //class doesn't have
+                Context c = new Context();
+                c.Run();
+            }
+        }
+
+        private static bool RunElevated(string fileName)
+        {
+            //MessageBox.Show("Run: " + fileName);
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.Verb = "runas";
+            processInfo.FileName = fileName;
+            try
+            {
+                Process.Start(processInfo);
+                return true;
+            }
+            catch (Win32Exception)
+            {
+                //Do nothing. Probably the user canceled the UAC window
+            }
+            return false;
         }
     }
 }
