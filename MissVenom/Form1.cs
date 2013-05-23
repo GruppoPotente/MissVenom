ed724eca6eb2bcc08db3ff853ff66409e5e6dd7b
Max Kovaljov <shirioko@gmail.com>
2013-05-23 22:02:32 +0200

    Lots of changes
    - Added logging (Finally!)
    - Added sniffing of ALL https requests to *.whatsapp.net, this includes:
    - v.whatsapp.net (registration)
    - sro.whatsapp.net (contact sync)
    - c*.whatsapp.net (chat host, just in case)
    - and probably many more!
    - bumped up version to 1.1.3.0
    
    experimental tcprelay is included but not enabled

--- MissVenom/Form1.cs
+++ MissVenom/Form1.cs
@@ -100,37 +100,105 @@ namespace MissVenom
             {
                 this.textBox1.AppendText(data + "\r\n");
                 this.textBox1.DeselectAll();
+                //log to file
+                try
+                {
+                    File.AppendAllLines("MissVenom.log", new String[] { data });
+                }
+                catch (Exception e)
+                {
+
+                }
             }
         }
 
         private void onHttpsRequest(object sender, RequestEventArgs e)
         {
-            String url = "https://v.whatsapp.net" + e.Request.Uri.PathAndQuery;
+            String url;
+            //dev, for test on localhost:
+            if (e.Request.Uri.Authority.EndsWith(".whatsapp.net"))
+            {
+                //use original
+                url = e.Request.Uri.AbsoluteUri;
+            }
+            else
+            {
+                //overwrite
+                url = "https://v.whatsapp.net" + e.Request.Uri.PathAndQuery;
+            }
+
             HttpWebRequest request = HttpWebRequest.Create(url) as HttpWebRequest;
             try
             {
-                request.UserAgent = e.Request.Headers["User-Agent"].HeaderValue;
+                request.Method = e.Request.Method;
+                if (e.Request.ContentType != null)
+                {
+                    request.ContentType = e.Request.ContentType.HeaderValue;
+                }
+                else
+                {
+                    request.ContentType = "application/x-www-form-urlencoded";
+                }
+                if (e.Request.ContentLength != null)
+                {
+                    request.ContentLength = long.Parse(e.Request.ContentLength.HeaderValue);
+                }
+                if (e.Request.Headers["User-Agent"] != null)
+                {
+                    request.UserAgent = e.Request.Headers["User-Agent"].HeaderValue;
+                }
+                if (e.Request.Headers["Accept"] != null)
+                {
+                    request.Accept = e.Request.Headers["Accept"].HeaderValue;
+                }
+                if (e.Request.Headers["Authorization"] != null)
+                {
+                    request.Headers.Add("Authorization", e.Request.Headers["Authorization"].HeaderValue);
+                }
+                if (e.Request.Headers["Accept-Encoding"] != null)
+                {
+                    request.Headers.Add("Accept-Encoding", e.Request.Headers["Accept-Encoding"].HeaderValue);
+                }
+                if (e.Request.Body.Length > 0)
+                {
+                    //copy body
+                    byte[] req = new byte[e.Request.Body.Length];
+                    e.Request.Body.Read(req, 0, (int)e.Request.Body.Length);
+                    Stream reqStream = request.GetRequestStream();
+                    reqStream.Write(req, 0, req.Length);
+                }
             }
             catch (Exception ex)
             {
                 this.AddListItem("Warning: " + ex.Message);
             }
-            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
-            e.Response.ContentType.Value = response.ContentType;
-            e.Response.ContentLength.Value = response.ContentLength;
+            try
+            {
+                HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+                e.Response.ContentType.Value = response.ContentType;
+                e.Response.ContentLength.Value = response.ContentLength;
+                if (response.Headers["WWW-Authenticate"] != null)
+                {
+                    e.Response.Add(new HttpServer.Headers.StringHeader("WWW-Authenticate", response.Headers["WWW-Authenticate"].ToString()));
+                }
 
-            Stream responseStream = response.GetResponseStream();
-            StreamReader responseReader = new StreamReader(responseStream);
-            String data = responseReader.ReadToEnd();
+                Stream responseStream = response.GetResponseStream();
+                StreamReader responseReader = new StreamReader(responseStream);
+                String data = responseReader.ReadToEnd();
 
-            byte[] data2 = Encoding.Default.GetBytes(data);
+                byte[] data2 = Encoding.Default.GetBytes(data);
 
-            e.Response.Body.Write(data2, 0, data2.Length);
-            
-            this.AddListItem("USERAGENT:" + e.Request.Headers["User-Agent"].HeaderValue);
-            this.AddListItem("REQUEST:  " + e.Request.Uri.AbsoluteUri);
-            this.AddListItem("RESPONSE: " + data);
-            this.AddListItem(" ");
+                e.Response.Body.Write(data2, 0, data2.Length);
+
+                this.AddListItem("USERAGENT:" + e.Request.Headers["User-Agent"].HeaderValue);
+                this.AddListItem("REQUEST:  " + e.Request.Uri.AbsoluteUri);
+                this.AddListItem("RESPONSE: " + data);
+                this.AddListItem(" ");
+            }
+            catch (Exception ex)
+            {
+                this.AddListItem("HTTPS REQUEST ERROR: " + ex.Message);
+            }
         }
 
         static System.Net.IPAddress GetIP()
@@ -257,9 +325,9 @@ namespace MissVenom
             }
 
             //start tcp relay
-            Thread tcpr = new Thread(new ThreadStart(startTcpRelay));
-            tcpr.IsBackground = true;
-            tcpr.Start();
+            //Thread tcpr = new Thread(new ThreadStart(startTcpRelay));
+            //tcpr.IsBackground = true;
+            //tcpr.Start();
 
             this.AddListItem("Set your DNS address on your phone to " + ips.First() + " (Settings->WiFi->Static IP->DNS)");
         }
@@ -267,7 +335,7 @@ namespace MissVenom
         private void startTcpRelay()
         {
             IPAddress ip = GetIP();
-            this.tcpl = new TcpListener(GetIP(), 5222);
+            this.tcpl = new TcpListener(IPAddress.Any, 5222);
             tcpl.Start();
             this.AddListItem("Started TCP relay, waiting for connection...");
             this.s_internal = this.tcpl.AcceptTcpClient();
@@ -299,9 +367,16 @@ namespace MissVenom
                     {
                         while (i > 0)
                         {
-                            to.Client.Send(buffer);
-                            this.bufferedMessages.Add(buffer);
-                            this.AddListItem(WhatsAppApi.WhatsApp.SYSEncoding.GetString(buffer));
+                            //trim
+                            int ii = buffer.Length - 1;
+                            while (buffer[ii] == 0)
+                                --i;
+                            byte[] bar = new byte[i + 1];
+                            Array.Copy(buffer, bar, i + 1);
+
+                            to.Client.Send(bar);
+                            //this.bufferedMessages.Add(bar);
+                            this.AddListItem(WhatsAppApi.WhatsApp.SYSEncoding.GetString(bar));
                             i = stream.Read(buffer, 0, buffer.Length);
                         }
                     }
