eb7a76a622e929fe75ac90959aa2d510e6a0302e
Max Kovaljov <shirioko@gmail.com>
2013-05-26 13:48:22 +0200

    Fixed TabIndex and AcceptButton

--- MissVenom/Form1.Designer.cs
+++ MissVenom/Form1.Designer.cs
@@ -46,7 +46,8 @@
             this.textBox1.ReadOnly = true;
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(261, 234);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TabStop = false;
             // 
             // textBox2
             // 
@@ -55,7 +56,7 @@
             this.textBox2.Location = new System.Drawing.Point(0, 3);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(212, 20);
-            this.textBox2.TabIndex = 2;
+            this.textBox2.TabIndex = 0;
             // 
             // button1
             // 
@@ -63,13 +64,14 @@
             this.button1.Location = new System.Drawing.Point(218, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(42, 23);
-            this.button1.TabIndex = 3;
+            this.button1.TabIndex = 1;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(261, 263);
