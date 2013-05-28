93d234b5b5a4e8ebdc802bfa617d6bee8adb2a8c
Max Kovaljov <shirioko@gmail.com>
2013-05-28 15:27:20 +0200

    Added registration json response class

---
+++ MissVenom/RegResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MissVenom
+{
+    public class RegResponse
+    {
+        public string status { get; set; }
+        public string login { get; set; }
+        public string pw { get; set; }
+        public string type { get; set; }
+        public string expiration { get; set; }
+        public string kind { get; set; }
+        public string price { get; set; }
+        public string cost { get; set; }
+        public string currency { get; set; }
+        public string price_expiration { get; set; }
+    }
+}
