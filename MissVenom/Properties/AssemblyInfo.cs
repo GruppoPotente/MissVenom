0cc3cb87bfc75ed3b016b3e16e02fb34f078d2a4
Max Kovaljov <shirioko@gmail.com>
2013-05-31 17:04:27 +0200

    Bumped up version to 1.2.3.0

--- MissVenom/Properties/AssemblyInfo.cs
+++ MissVenom/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MissVenom SSL")]
-[assembly: AssemblyDescription("MissVenom SSL proxy sniffer for WhatsApp")]
+[assembly: AssemblyTitle("MissVenom")]
+[assembly: AssemblyDescription("MissVenom proxy sniffer for WhatsApp")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("shirioko")]
 [assembly: AssemblyProduct("MissVenom")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+[assembly: AssemblyVersion("1.2.3.0")]
+[assembly: AssemblyFileVersion("1.2.3.0")]
